f2d0773feb21af18c11cf3a279a0c49c815b569d
marcosjsantos <marcosjsantosmjs@gmail.com>
2022-07-29 11:31:34 -0300

    entendendoJavaScript

---
+++ javascript/js/avengers.js
@@ -0,0 +1,15 @@
+// API 
+var avengers = ['Tony Stark', 'Clint barton', 'Natasha Romanoff', 'Steve Rogers', 'Bruce Banner', 'Scot Lang']
+
+function listaVingadores() {
+
+    var ul = document.getElementById('avengers')
+    ul.innerHTML = ''
+
+    avengers.forEach(function (a) {
+        var li = document.createElement('li')
+        var text = document.createTextNode(a)
+        li.appendChild(text)
+        ul.appendChild(li)
+    })
+}
---
+++ javascript/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.json({ message: 'Hello Cypress Discovery!' })
+})
+
+app.get('/avengers', function (req, res) {
+    var avengers = ['Tony Stark', 'Clint barton', 'Natasha Romanoff', 'Steve Rogers', 'Bruce Banner', 'Scot Lang']
+    return res.json({ data: avengers })
+})
+
+app.get('/cnh', function (req, res) {
+    const idade = req.query.idade
+
+
+    if (!idade) {
+        return res.json({ message: 'Idade é um campo obrigatório' })
+    }
+
+    var idadeNum = parseInt(idade)
+
+    if (idadeNum >= 18) {
+        return res.json({ message: 'Ok, você pode tirar sua CNH' })
+    } else if (idadeNum > 4) {
+        return res.json({ message: 'Você é menor de Idade por enquanto sugiro andar de bike.' })
+    } else {
+        return res.json({ message: 'Melhor você tomar leite....' })
+    }
+})
+
+app.listen(3000)
