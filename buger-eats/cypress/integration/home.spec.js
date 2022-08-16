b7d281ee8459e1f1bd663f1289ab57b33af7c2b2
marcosjsantos <marcosjsantosmjs@gmail.com>
2022-08-16 16:31:53 -0300

    AlteracaoDescricaoTeste

--- buger-eats/cypress/integration/home.spec.js
+++ buger-eats/cypress/integration/home.spec.js
@@ -1,5 +1,5 @@
 
-describe('Home Page', ()=>{
+describe('Home Page.', ()=>{
     it('App deve estar online', ()=>{
         cy.visit('/')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
