e106297ff9fb5e05b0e3b5830b92a8a9b7302b3c
marcosjsantos <marcosjsantosmjs@gmail.com>
2022-09-23 19:33:58 -0300

    ConfigurandoArquivocypressConfig.js

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   viewportHeight: 900,
   e2e: {
     baseUrl: 'https://buger-eats-qa.vercel.app/',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
