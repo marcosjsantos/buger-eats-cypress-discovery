4477af3d76ee23904b87dffe19c4139250045ab8
marcosjsantos <marcosjsantosmjs@gmail.com>
2022-09-23 19:21:02 -0300

    Configurando

--- cypress.config.js
+++ cypress.config.js
@@ -4,14 +4,13 @@ module.exports = defineConfig({
   projectId: 'uv5sga',
   viewportWidth: 1440,
   viewportHeight: 900,
-  specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   e2e: {
+    baseUrl: 'https://buger-eats-qa.vercel.app/',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'https://buger-eats-qa.vercel.app/',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 })
